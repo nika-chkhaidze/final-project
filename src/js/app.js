96c6d0c24257ad62de9b5caa7496f4b9aecaeb27
nika-chkhaidze <nikachkhaidze08@gmail.com>
2023-05-06 23:33:24 +0400

    part-2-update-1

--- src/js/app.js
+++ src/js/app.js
@@ -1,29 +1,74 @@
-var index = 0;
-var slides = document.querySelectorAll(".slides");
-var dot = document.querySelectorAll(".dot");
+window.onload = function() {
 
-function changeSlide(){
+    let slider = document.querySelector('#slider');
+    let move = document.querySelector('#move');
+    let moveLi = Array.from(document.querySelectorAll('#slider #move li'));
+    let forword = document.querySelector('#slider #forword');
+    let back = document.querySelector('#slider #back');
+    let counter = 1;
+    let time = 5000;
+    let dot;
 
-  if(index<0){
-    index = slides.length-1;
-  }
-  
-  if(index>slides.length-1){
-    index = 0;
-  }
-  
-  for(let i=0;i<slides.length;i++){
-    slides[i].style.display = "none";
-    dot[i].classList.remove("active");
-  }
-  
-  slides[index].style.display= "block";
-  dot[index].classList.add("active");
-  
-  index++;
-  
-  setTimeout(changeSlide,5000);
+    line.style.animation = 'line ' + (time / 5000) + 's linear infinite';
+
+    function moveUP() {
+
+        if (counter == moveLi.length) {
+
+            moveLi[0].style.marginLeft = '0%';
+            counter = 1;
+
+        } else if (counter >= 1) {
+
+            moveLi[0].style.marginLeft = '-' + counter * 100 + '%';
+            counter++;
+        } 
+
+    }
+
+    function moveDOWN() {
+
+        if (counter == 1) {
+
+            moveLi[0].style.marginLeft = '-' + (moveLi.length - 1) * 100 + '%';
+            counter = moveLi.length;
+
+        } else if (counter <= moveLi.length) {
+
+            counter = counter - 2;
+            moveLi[0].style.marginLeft = '-' + counter * 100 + '%';   
+            counter++;
+
+
+        }  
+
+    }
+
+
+    forword.onclick = moveUP;
+    back.onclick = moveDOWN;
+
+    let autoPlay = setInterval(moveUP, time);
+
+
+    slider.onmouseout = function() {
+
+        autoPlay = setInterval(moveUP, time);
+        line.style.animation = 'line ' + (time / 5000) + 's linear infinite';
+
+    }
+
+    
   
 }
 
-changeSlide();
+
+const element = document.querySelector('.progress-value-1');
+const observer = new IntersectionObserver(entries => {
+  element.classList.toggle('progress-value-1', entries[0].isIntersecting );
+});
+
+observer.observe( element );
+ 
+
+
